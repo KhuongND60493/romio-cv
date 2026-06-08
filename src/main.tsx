58b32fd388d93ae5ed02ed19c2e688d0b624571d
Romio Nguyen <nguyenromio@gmail.com>
2026-06-08 08:40:18 +0700

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from '@/features/theme'
-import { ModalProvider } from '@/features/modal'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from '@/features/theme';
+import { ModalProvider } from '@/features/modal';
 import '@/features/locales/i18n'
-import App from '@/App.tsx'
+import App from '@/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
